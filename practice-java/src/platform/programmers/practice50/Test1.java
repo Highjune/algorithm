b82a8dbe0523bf8538dc4a3313b439238f30e22a
highjune <highjune37@gmail.com>
2024-02-24 19:15:12 +0900

    문자열 곱하기

--- practice-java/src/platform/programmers/practice50/Test1.java
+++
@@ -1,7 +0,0 @@
-package platform.programmers.practice50;
-
-public class Test1 {
-	public static void main(String[] args) {
-		System.out.println("hhhh");
-	}
-}
---
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/문자열_곱하기.java
@@ -0,0 +1,2 @@
+package platform.programmers.코딩_기초_트레이닝;public class 문자열_곱하기 {
+}
