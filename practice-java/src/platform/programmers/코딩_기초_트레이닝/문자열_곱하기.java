3d522465370d15e53873877c2e836f3187fbbbf8
highjune <highjune37@gmail.com>
2024-02-25 16:45:32 +0900

    프로그래머스/문자열_곱하기

--- practice-java/src/platform/programmers/코딩_기초_트레이닝/문자열_곱하기.java
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/문자열_곱하기.java
@@ -1,2 +1,10 @@
-package platform.programmers.코딩_기초_트레이닝;public class 문자열_곱하기 {
+package platform.programmers.코딩_기초_트레이닝;
+public class 문자열_곱하기 {
+	public int solution(int num, int n) {
+		int answer = 0;
+
+		answer = num % n == 0 ? 1 : 0;
+		return answer;
+	}
+
 }
