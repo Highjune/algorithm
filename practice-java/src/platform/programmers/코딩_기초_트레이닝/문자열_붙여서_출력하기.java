ca8fe56927d8ecf351df5e18b68d83704f052e5e
highjune <highjune37@gmail.com>
2024-02-28 00:35:39 +0900

    문자열_붙여서_출력하기

---
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/문자열_붙여서_출력하기.java
@@ -0,0 +1,22 @@
+package platform.programmers.코딩_기초_트레이닝;
+
+import java.util.Scanner;
+
+public class 문자열_붙여서_출력하기 {
+	// public static void main(String[] args) {
+	// 	Scanner sc = new Scanner(System.in);
+	// 	String a = sc.next();
+	// 	String b = sc.next();
+	//
+	// 	System.out.println(a+b);
+	// }
+
+	// public static void main(String[] args) {
+	// 	Scanner sc = new Scanner(System.in);
+	// 	String a = sc.next();
+	// 	String b = sc.next();
+	//
+	// 	System.out.println(a.concat(b));
+	// }
+
+}
