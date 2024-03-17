8d617b3089956388d4aa0475f97ba135a9f8442c
highjune <highjune37@gmail.com>
2024-03-17 16:47:40 +0900

    문자열을_정수로_변환하기

---
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/문자열을_정수로_변환하기.java
@@ -0,0 +1,11 @@
+package platform.programmers.코딩_기초_트레이닝;
+
+public class 문자열을_정수로_변환하기 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
