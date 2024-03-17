21dcd0c631b14c21cae2473e8387f7cdd7afce07
highjune <highjune37@gmail.com>
2024-03-17 15:48:42 +0900

    소문자로 바꾸기 변경

--- practice-java/src/platform/programmers/코딩_기초_트레이닝/소문자로_바꾸기.java
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/소문자로_바꾸기.java
@@ -1,4 +1,15 @@
 package platform.programmers.코딩_기초_트레이닝;
 
 public class 소문자로_바꾸기 {
+
+    public static void main(String[] args) {
+        String myString = "mySTRING";
+        소문자로_바꾸기 test = new 소문자로_바꾸기();
+        System.out.println(test.solution(myString));
+    }
+
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+
 }
