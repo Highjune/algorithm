5f25a08a3a7d2a9fda2a0673c7ef7c2f737abfb4
highjune <highjune37@gmail.com>
2024-03-17 17:00:26 +0900

    길이에 따른 연산

--- practice-java/src/platform/programmers/코딩_기초_트레이닝/길이에_따른_연산.java
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/길이에_따른_연산.java
@@ -1,11 +1,13 @@
 package platform.programmers.코딩_기초_트레이닝;
 
+import java.util.stream.IntStream;
+
 public class 길이에_따른_연산 {
     public static void main(String[] args) {
 
     }
 
-    public int solution(int[] num_list) {
+    public int solution1(int[] num_list) {
         int result = 1;
         if (num_list.length >= 11) {
             for (int i : num_list) {
@@ -18,5 +20,10 @@ public class 길이에_따른_연산 {
             result *= i;
         }
         return result;
+    }
+
+    public int solution2(int[] num_list) {
+        return num_list.length >= 11 ? IntStream.of(num_list).sum()
+                : IntStream.of(num_list).reduce(1, (a, b) -> a * b);
     }
 }
