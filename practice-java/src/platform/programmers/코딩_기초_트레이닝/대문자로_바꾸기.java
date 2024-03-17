33abaa7d69475a3614827d8e81d97e039c7f2483
highjune <highjune37@gmail.com>
2024-03-17 16:52:51 +0900

    대문자로 바꾸기

---
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/대문자로_바꾸기.java
@@ -0,0 +1,11 @@
+package platform.programmers.코딩_기초_트레이닝;
+
+public class 대문자로_바꾸기 {
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+}
