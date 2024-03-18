7e58be8dac7c1a5864022c5bc0b89a4b709066c5
highjune <highjune37@gmail.com>
2024-03-18 23:59:43 +0900

    (푸는중) n개_간격의_원소들

--- practice-java/src/platform/programmers/코딩_기초_트레이닝/n개_간격의_원소들.java
+++ practice-java/src/platform/programmers/코딩_기초_트레이닝/n개_간격의_원소들.java
@@ -1,7 +1,22 @@
 package platform.programmers.코딩_기초_트레이닝;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class n개_간격의_원소들 {
     public static void main(String[] args) {
-               
+
+    }
+
+    public int[] solution(int[] num_list, int n) {
+        int[] result = {};
+        List<Integer> resultList = new ArrayList<>();
+
+        int size = 0;
+        for (int i = n - 1; i < num_list.length; i += n) {
+            size++;
+            resultList.add(num_list[i]);
+        }
+        resultList.toArray(new Integer[size]);
     }
 }
